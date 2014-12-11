066bb5d7193cc71744e11209636a3f52e4f40009
Rob Eisenberg <rob@bluespire.com>
2014-12-11 12:41:52 -0500

    fix(loader): incorrect paths passes to systemjs loader
    When calling System.import we need to add the System.baseUrl.

--- config.js
+++ config.js
@@ -7,10 +7,13 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.0.2",
-    "aurelia-metadata": "github:aurelia/metadata@0.0.5",
-    "github:aurelia/loader@0.0.2": {
-      "aurelia-path": "github:aurelia/path@0.0.1"
+    "aurelia-loader": "github:aurelia/loader@0.1.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.1.0",
+    "github:aurelia/loader@0.1.0": {
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.0",
+      "aurelia-path": "github:aurelia/path@0.1.0",
+      "es6-shim": "github:paulmillr/es6-shim@0.21.1",
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.1"
     }
   }
 });
