0462d8b2bbd1509181bf1b394a88b85e929a68a1
Rob Eisenberg <rob@bluespire.com>
2014-12-17 17:59:29 -0500

    fix(package): add missing dependency

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ System.config({
   "map": {
     "aurelia-loader": "github:aurelia/loader@0.2.0",
     "aurelia-metadata": "github:aurelia/metadata@0.1.1",
+    "aurelia-path": "github:aurelia/path@0.2.2",
     "github:aurelia/loader@0.2.0": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
