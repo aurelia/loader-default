9da71e4c9864138b3ce93bf6c8aa515086dfcfa8
Rob Eisenberg <rob@bluespire.com>
2014-12-17 16:42:25 -0500

    fix(package): update dependencies to latest versions

--- config.js
+++ config.js
@@ -7,11 +7,10 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.1.1",
-    "aurelia-metadata": "github:aurelia/metadata@0.1.0",
-    "github:aurelia/loader@0.1.1": {
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.0",
-      "aurelia-path": "github:aurelia/path@0.1.0",
+    "aurelia-loader": "github:aurelia/loader@0.2.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.1.1",
+    "github:aurelia/loader@0.2.0": {
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
       "es6-shim": "github:paulmillr/es6-shim@0.21.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     }
