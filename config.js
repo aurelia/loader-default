762fbfc9c463b612d0b286526735da138be90b78
Rob Eisenberg <rob@bluespire.com>
2015-01-06 12:16:42 -0500

    feat(build): update compiler and switch to register module format

--- config.js
+++ config.js
@@ -1,19 +1,27 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js"
+    "github:*": "jspm_packages/github/*.js",
+    "aurelia-loader-systemjs/*": "dist/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.2.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.1.1",
-    "aurelia-path": "github:aurelia/path@0.3.0",
-    "github:aurelia/loader@0.2.0": {
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.1",
-      "es6-shim": "github:paulmillr/es6-shim@0.21.1",
+    "aurelia-loader": "github:aurelia/loader@0.3.0",
+    "aurelia-metadata": "github:aurelia/metadata@0.2.3",
+    "aurelia-path": "github:aurelia/path@0.4.0",
+    "github:aurelia/loader@0.3.0": {
+      "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
+      "core-js": "npm:core-js@0.4.1",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
+    },
+    "github:jspm/nodelibs-process@0.1.0": {
+      "process": "npm:process@0.10.0"
+    },
+    "npm:core-js@0.4.1": {
+      "process": "github:jspm/nodelibs-process@0.1.0"
     }
   }
 });
