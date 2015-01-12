21511995ac06d523417bee3d9b061816479c0120
Rob Eisenberg <rob@bluespire.com>
2015-01-12 03:16:41 -0500

    fix(package): update Aurelia dependencies

--- config.js
+++ config.js
@@ -9,18 +9,18 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.3.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.2.3",
+    "aurelia-loader": "github:aurelia/loader@0.3.1",
+    "aurelia-metadata": "github:aurelia/metadata@0.2.4",
     "aurelia-path": "github:aurelia/path@0.4.0",
-    "github:aurelia/loader@0.3.0": {
+    "github:aurelia/loader@0.3.1": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
-      "core-js": "npm:core-js@0.4.1",
+      "core-js": "npm:core-js@0.4.4",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.2"
     },
     "github:jspm/nodelibs-process@0.1.0": {
       "process": "npm:process@0.10.0"
     },
-    "npm:core-js@0.4.1": {
+    "npm:core-js@0.4.4": {
       "process": "github:jspm/nodelibs-process@0.1.0"
     }
   }
