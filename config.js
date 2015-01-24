5c5b501676550e0f38dec195af6348bece39e0d1
Rob Eisenberg <rob@bluespire.com>
2015-01-24 01:02:54 -0500

    fix(package): update reps and fix bower semver ranges

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.3.2",
+    "aurelia-loader": "github:aurelia/loader@0.3.3",
     "aurelia-metadata": "github:aurelia/metadata@0.3.0",
     "aurelia-path": "github:aurelia/path@0.4.1",
-    "github:aurelia/loader@0.3.2": {
+    "github:aurelia/loader@0.3.3": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.1.2",
       "core-js": "npm:core-js@0.4.6",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.3"
+      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.5.4"
     },
     "github:jspm/nodelibs-process@0.1.0": {
       "process": "npm:process@0.10.0"
