c0235607ce325b5ccba8ce99b73d560c72e3a1b1
Rob Eisenberg <rob@bluespire.com>
2015-04-30 01:13:48 -0400

    chore(all): update compilation process and readme

--- karma.conf.js
+++ karma.conf.js
@@ -41,6 +41,7 @@ module.exports = function(config) {
         sourceMap: 'inline',
         modules: 'system',
         moduleIds: false,
+        loose: "all",
         optional: [
           "es7.decorators"
         ]
