8c462157d250c277d838c2f7222b78a132290d30
Rob Eisenberg <rob@bluespire.com>
2015-07-29 17:28:19 -0400

    chore(build): improve output file name

--- config.js
+++ config.js
@@ -16,24 +16,19 @@ System.config({
 
 System.config({
   "map": {
-    "aurelia-loader": "github:aurelia/loader@0.8.0",
-    "aurelia-metadata": "github:aurelia/metadata@0.7.0",
+    "aurelia-loader": "github:aurelia/loader@0.8.3",
+    "aurelia-metadata": "github:aurelia/metadata@0.7.1",
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
     "core-js": "npm:core-js@0.9.18",
-    "github:aurelia/loader@0.7.0": {
+    "github:aurelia/loader@0.8.3": {
       "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-      "aurelia-path": "github:aurelia/path@0.7.0",
+      "aurelia-metadata": "github:aurelia/metadata@0.7.1",
+      "aurelia-path": "github:aurelia/path@0.8.1",
       "core-js": "npm:core-js@0.9.18",
       "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
     },
-    "github:aurelia/loader@0.8.0": {
-      "aurelia-html-template-element": "github:aurelia/html-template-element@0.2.0",
-      "aurelia-path": "github:aurelia/path@0.8.0",
-      "core-js": "npm:core-js@0.9.18",
-      "webcomponentsjs": "github:webcomponents/webcomponentsjs@0.6.3"
-    },
-    "github:aurelia/metadata@0.7.0": {
+    "github:aurelia/metadata@0.7.1": {
       "core-js": "npm:core-js@0.9.18"
     },
     "github:jspm/nodelibs-process@0.1.1": {
