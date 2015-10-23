4011cd908125efe6f3d1b4faacc939a0309de7ce
Rob Eisenberg <rob@bluespire.com>
2015-10-23 15:26:18 -0400

    doc(all): complete api docs

--- dist/aurelia-loader-default.js
+++ dist/aurelia-loader-default.js
@@ -2,7 +2,16 @@ import {TemplateRegistryEntry,Loader} from 'aurelia-loader';
 import {DOM,PLATFORM} from 'aurelia-pal';
 import {Origin} from 'aurelia-metadata';
 
+/**
+* An implementation of the TemplateLoader interface implemented with text-based loading.
+*/
 export class TextTemplateLoader {
+  /**
+  * Loads a template.
+  * @param loader The loader that is requesting the template load.
+  * @param entry The TemplateRegistryEntry to load and populate with a template.
+  * @return A promise which resolves when the TemplateRegistryEntry is loaded with a template.
+  */
   loadTemplate(loader: Loader, entry: TemplateRegistryEntry): Promise<any> {
     return loader.loadText(entry.address).then(text => {
       entry.setTemplate(DOM.createTemplateFromMarkup(text));
@@ -33,13 +42,31 @@ function ensureOriginOnExports(executed, name) {
   return executed;
 }
 
+/**
+* Represents a template loader.
+*/
 interface TemplateLoader {
+  /**
+  * Loads a template.
+  * @param loader The loader that is requesting the template load.
+  * @param entry The TemplateRegistryEntry to load and populate with a template.
+  * @return A promise which resolves when the TemplateRegistryEntry is loaded with a template.
+  */
   loadTemplate(loader: Loader, entry: TemplateRegistryEntry): Promise<any>;
 }
 
+/**
+* A default implementation of the Loader abstraction which works with SystemJS, RequireJS and Dojo Loader.
+*/
 export class DefaultLoader extends Loader {
+  /**
+  * The name of the underlying native loader plugin used to load text.
+  */
   textPluginName: string = 'text';
 
+  /**
+  * Creates an instance of the DefaultLoader.
+  */
   constructor() {
     super();
 
@@ -56,10 +83,19 @@ export class DefaultLoader extends Loader {
     });
   }
 
+  /**
+  * Instructs the loader to use a specific TemplateLoader instance for loading templates
+  * @param templateLoader The instance of TemplateLoader to use for loading templates.
+  */
   useTemplateLoader(templateLoader: TemplateLoader): void {
     this.templateLoader = templateLoader;
   }
 
+  /**
+  * Loads a collection of modules.
+  * @param ids The set of module ids to load.
+  * @return A Promise for an array of loaded modules.
+  */
   loadAllModules(ids: string[]): Promise<any[]> {
     let loads = [];
 
@@ -70,10 +106,20 @@ export class DefaultLoader extends Loader {
     return Promise.all(loads);
   }
 
+  /**
+  * Loads a template.
+  * @param url The url of the template to load.
+  * @return A Promise for a TemplateRegistryEntry containing the template.
+  */
   loadTemplate(url: string): Promise<TemplateRegistryEntry> {
     return this._import(this.applyPluginToUrl(url, 'template-registry-entry'));
   }
 
+  /**
+  * Loads a text-based resource.
+  * @param url The url of the text file to load.
+  * @return A Promise for text content.
+  */
   loadText(url: string): Promise<string> {
     return this._import(this.applyPluginToUrl(url, this.textPluginName));
   }
--- src/text-template-loader.js
+++ src/text-template-loader.js
@@ -1,7 +1,16 @@
 import {TemplateRegistryEntry, Loader} from 'aurelia-loader';
 import {DOM} from 'aurelia-pal';
 
+/**
+* An implementation of the TemplateLoader interface implemented with text-based loading.
+*/
 export class TextTemplateLoader {
+  /**
+  * Loads a template.
+  * @param loader The loader that is requesting the template load.
+  * @param entry The TemplateRegistryEntry to load and populate with a template.
+  * @return A promise which resolves when the TemplateRegistryEntry is loaded with a template.
+  */
   loadTemplate(loader: Loader, entry: TemplateRegistryEntry): Promise<any> {
     return loader.loadText(entry.address).then(text => {
       entry.setTemplate(DOM.createTemplateFromMarkup(text));
